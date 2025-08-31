b09e06382d82b0f99eaf204b4343269529200019
LooJinnYang <ljinnyang@gmail.com>
2025-08-31 03:39:52 +0000

    31/8

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+function setup(){
+ createCanvas(600, 400);
+ background(0);
+}
 
+function draw() {
+circle(150, height/2, 100);
+fill("black");
+stroke("red");
+strokeWeight(10);
+
+triangle(width/2, height/2 - 50, width/2 + 50, height/2 + 50, width/2 - 50, height/2 + 50);
+fill("black");
+stroke("red");
+strokeWeight(10);
+
+rect(width/2 + 100, height/2 - 50, 100, 100);
+fill("black");
+stroke("red");
+strokeWeight(10);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,13 @@
+function setup(){
+ createCanvas(600, 600);
+ background(220);
+}
 
+function draw() {
+
+for (let i = 0; i < 10; i++){
+    let shade = i * 50 + 50
+    fill(shade);
+    ellipse(i * 50 + 50 , i *50 + 50 , 50 , 50);
+}
+}
